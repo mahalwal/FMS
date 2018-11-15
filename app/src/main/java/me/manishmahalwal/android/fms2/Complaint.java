6ef3fed9210b65ba66b1ce8ba504495bfb1f8522
Madhav Thakker <madhav16159@iiitd.ac.in>
2018-11-15 20:43:20 +0530

    Adding Complaint.java

---
+++ app/src/main/java/me/manishmahalwal/android/fms2/Complaint.java
@@ -0,0 +1,7 @@
+package me.manishmahalwal.android.fms2;
+
+public class Complaint {
+    public String complaintNum, complaintType, complaintFrom, complaitTo;
+    public int priority;
+    public String complaintRoom;
+}
